c523f480330a929738de5fa495bd04cbe8e5f36d
Han Han <haannn@qq.com>
2020-08-26 10:48:12 +0800

    code环境配置

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+  const profile = {
+    host: 'localhost',
+    port: 3000,
+  };
+  const server = Hapi.server(profile);
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (req, h) => 'Hello hapi.js',
+  });
+  await server.start();
+  console.log('Server is running on %s', server.info.uri);
+};
+
+init().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
