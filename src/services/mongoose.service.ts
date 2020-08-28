b6dc79e847262d3f1d2e355265d73a741c642ab6
Han Han <haannn@qq.com>
2020-08-28 09:36:10 +0800

    refector mongoose service

--- src/services/mongoose.service.ts
+++ src/services/mongoose.service.ts
@@ -1,15 +1,29 @@
 import { mongoose } from '@typegoose/typegoose';
 
-const userProfile = {
+interface userProfileInterface {
+  host: string;
+  user: string;
+  password: string;
+  port: string | number;
+}
+function getUri(p: userProfileInterface) {
+  let result = 'mongodb://';
+  if (p.password || p.user) {
+    result += `${p.user}:${p.password}@`;
+  }
+  result += p.host;
+  if (p.port) {
+    result += `:${p.port}`;
+  }
+  return result;
+}
+const userProfile: userProfileInterface = {
   host: process.env.MONGO_HOST || 'localhost',
   user: process.env.MONGO_USER || 'root',
   password: process.env.MONGO_PASSWORD || 'password',
   port: process.env.MONGO_PORT || 27017,
 };
-function getUri(p: any) {
-  const uri = `mongodb://${p.user}:${p.password}@${p.host}:${p.port}`;
-  return uri;
-}
+
 const uri = getUri(userProfile);
 const profile: mongoose.ConnectionOptions = {
   useNewUrlParser: true,
